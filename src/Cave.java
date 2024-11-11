526f1643bc6c9839dd26f9d9678917f81a58b877
Kelvin Liu <kelvinyliu@gmail.com>
2024-11-11 20:27:10 +0000

    Appended a new attribute to the Cave class

--- src/Cave.java
+++ src/Cave.java
@@ -4,7 +4,7 @@ public class Cave
 {
     Entity containingEntity;
     Cave nextCave;
-    Cave parentCave;
     boolean isBranch;
+    Integer mainCaveNumber; // Used to set the randomised branch number to make the BRANCH_SCANNER function.
     KelvinList<Cave> branches = new KelvinList<Cave>();
 }
